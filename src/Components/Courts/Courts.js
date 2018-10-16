6e1c828b905bfc5c979bb420aca8561da1bc2cec
David Perdomo <daeperdomocr@unal.edu.co>
2018-10-16 23:51:27 +0000

    routing update

--- src/Components/Courts/Courts.js
+++ src/Components/Courts/Courts.js
@@ -5,29 +5,27 @@ import {Row, Col, Button, Card, CardTitle} from 'react-materialize'
 export default class Courts extends Component {
  render() {
     return (
-    <div>
-    <div>
+      <div>
         <Row>
           <Col l={3} className='grid-example'></Col>
           <Col l={6} className='grid-example'>
             <Card className=''>
-                <h4><b>Canchas</b></h4>
-  				Regístrate como jugador para obtener la calificación de otros usuarios, comparte tu disponibilidad de tiempo para jugar y tus estadísticas.<br/>
-				Crea equipos, añade miembros y busca jugadores!<br/>
-				Consulta canchas disponibles para jugar!
+              <h4><b>Canchas</b></h4>
+  				    Regístrate como jugador para obtener la calificación de otros usuarios, comparte tu disponibilidad de tiempo para jugar y tus estadísticas.<br/>
+				      Crea equipos, añade miembros y busca jugadores!<br/>
+				      Consulta canchas disponibles para jugar!
             </Card>
           </Col>
         </Row>
         <div style={{'margin-bottom':'200px'}}>
-            <Button floating fab='vertical' faicon='fa fa-plus' className='red' icon='add' large style={{bottom: '45px', right: '24px'}}>
-                <Button floating icon='insert_chart' className='red'/>
-                <Button floating icon='format_quote' className='yellow darken-1'/>
-                <Button floating icon='publish' className='green'/>
-                <Button floating icon='attach_file' className='blue'/>
-            </Button>
+          <Button floating fab='vertical' faicon='fa fa-plus' className='red' icon='add' large style={{bottom: '45px', right: '24px'}}>
+            <Button floating icon='insert_chart' className='red'/>
+            <Button floating icon='format_quote' className='yellow darken-1'/>
+            <Button floating icon='publish' className='green'/>
+            <Button floating icon='attach_file' className='blue'/>
+          </Button>
         </div>
-    </div>
-    </div>
+      </div>
     )
   }
 }
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -3,14 +3,14 @@ import {Footer} from 'react-materialize'
 
 export default props => 
     <div>
-    <Footer copyrights="2018 Picaditos2.0"
-      links={
-        <ul>
-          <li><a className="grey-text text-lighten-3" href="#!">Equipos</a></li>
-          <li><a className="grey-text text-lighten-3" href="#!">Partidos</a></li>
-          <li><a className="grey-text text-lighten-3" href="#!">Canchas</a></li>
-        </ul>
-      }
-    >
-    </Footer>
+      <Footer copyrights="2018 Picaditos2.0"
+        links={
+          <ul>
+            <li><a className="grey-text text-lighten-3" href="#!">Equipos</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Partidos</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Canchas</a></li>
+          </ul>
+        }
+      >
+      </Footer>
     </div>
