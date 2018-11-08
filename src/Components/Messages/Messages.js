449a925a058677470e7066c3a43f463bb9e24e68
Edgar David Garay Forero <edgarayf@unal.edu.co>
2018-11-08 01:44:20 +0000

    mensajes v3

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,70 +1,78 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import MessageS from './MessageS';
 import {Row, Col, Button, Card, CardTitle} from 'react-materialize'
-import { Switch, Route } from 'react-router-dom'
 import axios from 'axios';
+import * as consts from '../../consts';
 var a;
 
 export default class Messages extends Component {
      state = {
     eventos: []
   }
-   componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/users `, {
-            query: {
-            ID: 12345
-            }
-        })
-        .then(res => {
-            const eventos = res.data;
-      	    const b = eventos[0].name;
-            this.setState({ eventos });
-            a=JSON.stringify(b)
-            console.log(a)
-        })
-    }
-    
- render() {
-    return (
-    <div>
-    { this.state.eventos.map(evento =>
-    <div>
-        <Row>
-          <Col l={3} className='grid-example'></Col>
-          <Col l={6} className='grid-example'>
-            <Card className=''>
-                <h1><b>Mensaje de {evento.username}</b></h1>
-  			    	<p>
-		  		    {evento.email}
-		  	        </p>
-            </Card>
-          </Col>
-        </Row>
-    </div>
-    )}
-    <Row>
-        <Col l={3} className='grid-example'></Col>
-          <Col l={6} className='grid-example'>
-            
-            <Card className=''>
-               <Button floating icon='add' className='red' href="/Canchas"/> Nuevo mensaje     
-            </Card>
-        </Col>
-    </Row>
-        <div style={{'margin-bottom':'200px'}}>
-            <Button floating fab='vertical' faicon='fa fa-plus' className='red' icon='add' large style={{bottom: '45px', right: '24px'}}>
-                <Button floating icon='insert_chart' className='red'/>
-                <Button floating icon='format_quote' className='yellow darken-1'/>
-                <Button floating icon='publish' className='green'/>
-                <Button floating icon='attach_file' className='blue'/>
-            </Button>
-        </div>    
+  componentDidMount() {
+    console.log(JSON.parse(sessionStorage.user).username)
+    axios.post(consts.GRAPHQL_URL, {
+    //axios.get(`http://192.168.99.102:4003/message`)
+    //,{
+      "query":"query{\n  messageByReceptor(username:\""+JSON.parse(sessionStorage.user).username+"\"){\n    user1\n    subject\n    content\n  }\n}","variables":null
+    })
  
-    </div>
-    )
-  }
-}
+    .then(res => {
+        const eventos = res.data.data.messageByReceptor;
+        console.log(res.data.data)        
+        this.setState({ eventos });
+        //a=JSON.stringify(b)
+        console.log(eventos)
+        })
+    .catch(function (error) {
+        console.log(error)
+    });
+    }
+    //<a href="/MensajeNL" class="btn btn-primary green darken-4">Enviados</a>
+    render() {
+        return (
+        <div>
+          <Row>
+            <Col l={2} m={1} className='grid-example'></Col>
+            <Col l={8} m={10} s={12} className='grid-example'>
+            <br></br>
+              <Card className='' textClassName=''>
+                <h4 font ><b>Mensajes recibidos</b></h4>
+
+                
+          
+                
+              </Card> 
+              
+                <Row>
+                <Card className='' textClassName=''> 
+                  { this.state.eventos.map(evento =>
+                  <div>
+                   
+                      <Card className='' textClassName=''>
+                      <h5>Mensaje de {evento.user1}</h5>
+                      <p>{evento.subject}</p>
+                      </Card>
+
+                  </div>
+                 )}
+                   
+                </Card>
+                </Row>
+                <MessageS />                             
+            </Col>
+          </Row>
+          
+        </div>
+        )
+      }
+    }
 
 
 
+
+
+ 
+ 
 
