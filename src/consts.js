f74915b22a11e19fc7ec3b1f012da1e21e7a3d6e
davideperdomo <daeperdomocr@unal.edu.co>
2019-02-07 18:46:45 -0500

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const GRAPHQL_URL = process.env.API_URL + process.env.API_PORT + process.env.API_ENTRY
+export const GRAPHQL_URL = 'http://' + process.env.API_URL + ':' + process.env.API_PORT + '/' + process.env.API_ENTRY
 //export const GRAPHQL_URL = "http://192.168.99.101:5000/graphql"
 export const url = process.env.API_URL
 export const port = process.env.API_PORT
