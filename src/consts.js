92bce3b283b2faf3d406b052030dc5da9fce830a
dehormazah <dehormazah@unal.edu.co>
2019-02-09 17:00:19 -0500

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_URL = 'http://picaditos-api:5000/graphql'
 //export const GRAPHQL_URL = "http://192.168.99.101:5000/graphql"
-console.log("HOST: "+ GRAPHQL_URL + " url:" + url)
+console.log("HOST: "+ GRAPHQL_URL)
