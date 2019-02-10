c4620c5481db70ba8797ca2ca6e0e629f37045f3
dehormazah <dehormazah@unal.edu.co>
2019-02-09 21:36:45 -0500

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,2 @@
-export const GRAPHQL_URL = 'http://35.200.163.198:3030/graphql'
-//export const GRAPHQL_URL = "http://192.168.99.101:5000/graphql"
+export const GRAPHQL_URL = 'http://35.200.163.198:5000/graphql'
 console.log("HOST: "+ GRAPHQL_URL)
